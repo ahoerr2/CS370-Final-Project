0e62f4d90104c42d1e266cad5a07e06c28810113
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2023-04-15 20:43:11 -0500

    Added Data Dictionary

---
+++ data_dictionary.php
@@ -0,0 +1,8 @@
+<?php include_once('header.php') ; ?>
+<div class="container mt-3">
+    <h1 class="text-lg-center text-center text-white"> Data Dictionary</h1>
+    <!--Paste Markdown Code Here-->
+</div>
+
+<?php include_once('footer.php') ; ?>
+
