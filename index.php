406aec9abfaf0e79cb79eb0f8e60c64f8ae03f64
kait <130717246+angelkait@users.noreply.github.com>
2023-05-01 15:07:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include_once('header.php') ; ?>
     <div class="p-5">
-        <h1 class="text-light">Pizza Home</h1>
+        <h1 class="text-light">Seekr Home</h1>
         <p>
-            There’s nothing cookie-cutter about Pizza Hut.
+            An advanced social media platform for all professionals.
         </p>
 <?php include_once('footer.php') ; ?>
