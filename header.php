6defb2e8a2b6b87e2b085aa413a83c404f27079b
kait <130717246+angelkait@users.noreply.github.com>
2023-05-01 15:19:54 -0500

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
-<body class="bg-dark">
+<body class="bg-blue">
 <div class="bg-white">
 <ul class = "nav" style="padding-top: 15px; padding-left: 420px">
     <li class = "nav-item"><a class = "nav-link" href="index.php">Home</a></li>
