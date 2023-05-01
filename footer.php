52b8ff222b096474ef9a9fca392633ed5bf3380f
kait <130717246+angelkait@users.noreply.github.com>
2023-05-01 15:09:07 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="bg-dark text-white text-center text-lg-center fixed-bottom">
     <br/>
-    <p>Copyright &copy; 2023 Hoerr</p>
+    <p>Copyright &copy; 2023 Hoerr, Rebhan, Reyes, Montano</p>
 </div>
 </body>
 </html>
